f87b2e8bc4ec26f051d094cd8b825715b4cf885c
Alzeks <89770121+Alzeks@users.noreply.github.com>
2024-07-04 12:58:14 +0300

    Update page.tsx

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import DishesItem from "../components/DishesItem";
 import Search from "../components/Search";
 //api Lookup a single random meal   www.themealdb.com/api/json/v1/1/random.php
 //api by letter 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata'
 //api          'https://www.themealdb.com/api/json/v1/1/randomselection.php'
 
-async function getData(option) {
+interface Item{
+  strMealThumb: string,
+  strMeal: string,
+  strInstructions: string
+}
+async function getData(option: string) {
   if (!option) { option = 'a' }
   const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${option}`,
     //{cache: "no-store",}
@@ -17,20 +21,29 @@ async function getData(option) {
   return res.json();
 }
 
-export default async function Menu({ searchParams }) {
+export default async function Menu({searchParams}) {
   const q = searchParams?.query || '';
-  const data = await getData(q);
+
+  const {meals} = await getData(q);
 
   return (
     <main className={styles.container}>
       Menu
       <Search />
-      <div className="input">
-      </div>
-      <div className={styles.dishes}>
-        {data.meals.map((item: {}, i: string) =>
-          <DishesItem item={item} key={i} //becorse no manipulation 
+      <div className={styles.disheswraper}>
+        {meals.map((item: Item, i: string) =>
+        <div className={styles.dishes}>
+        <div className={styles.dishes_img}>
+          <Image src={item.strMealThumb} alt='img'
+            width={540}
+            height={301}
           />
+        </div>
+        <div className={styles.dishes_text}>
+          <h3>{item.strMeal}</h3>
+          <p>{item.strInstructions}</p>
+        </div>
+      </div>
         )
         }
       </div>
